a1c993a93c364a142a4164d02f61f3d638c62d9f
Daria Zherebtsova <dazhe2309@yandex.ru>
2021-05-28 18:20:12 +0300

    configure Express

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
-
-app.use(express.static('./dist/'));
+console.log('__dirname', __dirname);
+app.use("/", express.static(__dirname +'/dist'));
+app.use("/auth", express.static(__dirname + '/dist/authorization'));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
--- src/pages/main/index.hbs.js
+++ src/pages/main/index.hbs.js
@@ -1,4 +1,3 @@
 export const mainTempl = `
-<h1>{{prefix}}</h1>
 {{> chatList chats=chats}}
 `
